fb775fb89c99248c7cff445b94326c51ca466f67
gksouza <gkoliver2020@gmail.com>
2025-08-27 22:46:15 -0300

    changing alt by JavaScript

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  
   // pegar a tag img
   const img = document.querySelector("#profile img")
-  
+  // substituir a imagem  
   if(html.classList.contains('light')) {
-    // substituir a imagem
     // se tiver light mode, adicionar a imagem light
     img.setAttribute('src', './assets/avatar-light.png')
-    
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute('src', './assets/avatar-dark.png')
+  }
+
+  const alt = document.querySelector("#profile")
+  if(html.classList.contains('light')) {
+    alt.setAttribute("src", "Foto de GK sorrindo com fundo de bolinhas vermelhas")
+  } else {
+    alt.setAttribute("src", "Foto de GK sorrindo com fundo de bolinhas brancas")
   }
 }
 
