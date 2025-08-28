1ca5ba814d3f8cf38cb42029d55d1c45606f8ed5
gksouza <gkoliver2020@gmail.com>
2025-08-27 22:12:58 -0300

    updating HTML attributes with JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 function toggleMode() {
   const html = document.documentElement
-  // if(html.classList.contains('light')) {
+  html.classList.toggle("light")
+  
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  
+  if(html.classList.contains('light')) {
+    // substituir a imagem
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+    
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', './assets/avatar-dark.png')
+  }
+}
+
+// if(html.classList.contains('light')) {
   //   html.classList.remove('light')  
   // } else {
   //   html.classList.add('light')
   // }
-  html.classList.toggle('light')
-}
